14f44506976800282121e442cfaf7410bda1679d
gffuma <fumagalli.gf@gmail.com>
2018-02-02 15:18:22 +0100

    Adding token selectors

--- src/auth/selectors.js
+++ src/auth/selectors.js
@@ -3,3 +3,7 @@ export const isLoginLoading = state => state.auth.loginLoading
 export const getLoginError = state => state.auth.loginError
 
 export const getAuthUser = state => state.auth.user
+
+export const getAuthAccessToken = state => state.auth.accessToken
+
+export const getAuthRefreshToken = state => state.auth.refreshToken
